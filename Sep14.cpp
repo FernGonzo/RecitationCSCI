a3eb09a83223eec4848b620490b08d0069546e68
Fernando <fego9376@colorado.edu>
2023-09-14 17:08:49 +0000

    Update

---
+++ Sep14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    // memory allocation
+    
+    //Static vs dynamic memory allocation
+    // dyanmic memory allocation
+    int *ptr1 = new int;
+    int *ptr2 = new int[10];
+    
+    // Pass by value
+    // pass by pointers
+    // pass by reference
+    
+    return 0;
+}
